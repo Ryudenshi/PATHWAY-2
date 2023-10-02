bf320f961eec9e3ef4637428640c7b4a9c0884ab
Ryudenshi <skarydog17@gmail.com>
2023-10-02 15:17:49 +0300

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PATHWA_2_Lets_Evgen_IPZm-11"
+include(":app")
